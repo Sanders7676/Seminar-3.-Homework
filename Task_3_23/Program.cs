c20ee5c7bf15bc802f1be8ff9afb7aea61ce1ae2
Sanders76 <sanders76@list.ru>
2022-12-02 20:40:17 +0300

    Changes made to the code of Task_2_23

--- Task_3_23/Program.cs
+++ Task_3_23/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+//Задание должно быть выполнено в методе и метод должен вернуть массив чисел.
+
+
+Console.Write("Введите число: ");
+
+int num = InputInt();
+
+int[] cubesTable = GetTableOfCubesOfNumbers(num);
+
+Console.Write($"Кубы чисел от 1 до {num}: | ");
+
+PrintArray(cubesTable);
+
+
+// Метод для приема числа, вводимого черз консоль
+
+int InputInt()
+{
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение");
+        return -1;
+    }
+}
+
+
+// Метод формирования списка кубов чисел от 1 до N
+
+int[] GetTableOfCubesOfNumbers(int num)
+{
+    int[] array = new int[num];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = (int)(Math.Pow(i + 1, 3));
+    }
+
+    return array;
+}
+
+
+// Метод печати Массива
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"{array[i]} | ");
+    }
+}
