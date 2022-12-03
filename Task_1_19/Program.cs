5fc8fc9f97bdc7e4ff20da65705c7e59a9ae2b1f
Sanders76 <sanders76@list.ru>
2022-12-03 10:30:21 +0300

    Changes made to the code of Task_1_19

--- Task_1_19/Program.cs
+++ Task_1_19/Program.cs
@@ -5,7 +5,13 @@
 Console.Write("Введите пятизначное число: ");
 int num = InputInt();			                // Запускается метод приема 5-значного числа, вводимого через консоль
 int[] palindrom = FillArray(num);	            // Запускается метод заполнения массива из знаков 5-значного числа
-IfPalindrom(palindrom);                         // Запускается метод проверки на палиндром
+
+
+
+
+
+//IfPalindrom(palindrom);                         // Запускается метод проверки на палиндром
+PrintPalindrom(palindrom);
 
 // Метод для приема пятизначного числа, вводимого через консоль
 
@@ -30,22 +36,53 @@ int[] FillArray(int num)
 {
     int[] array = new int[5];    // {0,0,0,0,0}
     for (int temp = num, i = 5; i >= 1; temp = temp / 10, i = i - 1)
-	{
-    array[i - 1] = temp % 10;
+    {
+        array[i - 1] = temp % 10;
     }
     return array;
 }
 
+
 // Метод проверки на палиндром
 
-void IfPalindrom(int[] Palindrom)
+void IfPalindrom(int[] palindrom)
+// {
+//     if (palindrom[0] == palindrom[4] && palindrom[1] == palindrom[3])
+//     {
+//         Console.WriteLine($"Число {num} является палиндромом.");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Число {num} не является палиндромом.");
+//     }
+// }
 {
-    if (palindrom[0] == palindrom[4] && palindrom[1] == palindrom[3])
+    int[] ifPalindrom = new int[palindrom.Length];
+
+    int size = palindrom.Length;
+
+    for (int i = 0; i <= palindrom.Length - 1; i++, size = size - 1)
+    {
+        ifPalindrom[i] = palindrom[size - 1];
+    }
+
+    if (palindrom == ifPalindrom)
     {
         Console.WriteLine($"Число {num} является палиндромом.");
     }
     else
     {
         Console.WriteLine($"Число {num} не является палиндромом.");
+    }
+}
+
+
+void PrintPalindrom(int[] palindrom)
+{
+    int count = palindrom.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{palindrom[i]} ");
+        //Console.Write($"{ifPalindrom[i]} ");
     }
 }
